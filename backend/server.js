db32b9119349d4e673d5a54e1c8465a439185c57
Tiago Silva <tiago.silva@impactzero.pt>
2019-05-31 13:29:34 +0100

    stuff

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.use(cors());
 var mongoose = require('mongoose');
 var nodemailer = require('nodemailer');
 
+let uiBaseURL = "http://localhost:3000";
+
 // Email transporter
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -19,6 +21,7 @@ var transporter = nodemailer.createTransport({
     tls: { rejectUnauthorized: false }
 });
 
+console.log(module.uiBaseURL);
 
 // DB Connection
 mongoose.connect('mongodb://localhost/webdb', { useNewUrlParser: true });
@@ -32,9 +35,7 @@ db.on('open', () => {
     console.log("Connected to Database");
 
     var requestLoop = setInterval(function () {
-        console.log("GETTING ISSUES...");
         let url = apiBaseURL + "/api/v1/issues?after=" + new Date("05 October 2011 14:48 UTC").toISOString() + "&limit=1";
-        console.log(url);
         request({
             url: url,
             method: "GET"
@@ -67,12 +68,12 @@ db.on('open', () => {
                                     let client = splitName[1].trim();
 
                                     // Send email for each user
-                                    let body = "<html><head> <meta charset=\"UTF-8\"> <title>Inqu\u00E9rito de Satisfa\u00E7\u00E3o</title> <!-- Font Awesome Icon Library --> <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"> <style> .title { text-align: center; font-family: Arial, Helvetica, sans-serif; } #presentation { /*border-style: solid;*/ width: 50%; margin-right: auto; margin-left: auto; } #name { font-style: italic; font-weight: bold; } .req_details { /*border-style: solid;*/ width: 50%; margin-right: auto; margin-left: auto; } #client, #product, #subject, #timestamp, #operator { color: red; font-weight: bold; } .inq { /*border-style: solid;*/ width: 30%; margin-right: auto; margin-left: auto; padding-right: 10%; text-align: right; } .links { color: black; text-decoration: none; } .checked { color: orange; } #goodbye { text-align: right; margin-right: 10%; } </style></head><body> <h1 class=\"title\">Inqu\u00E9rio de Satisfa\u00E7\u00E3o - Sistema de Gest\u00E3o de Qualidade</h1> </br> <div id=\"presentation\"> <p>Caro(a) <span id=\"name\">" + user.firstname + " " + user.lastname + "</span>,</p> <p>este inqu\u00E9rito visa avaliar a qualidade do servi\u00E7o de apoio ao cliente da nossa empresa - <b>JIBS</b>,</p> <p>tendo por base a sua experi\u00EAncia no \u00E2mbito dos diferentes contactos feitos com os nossos servi\u00E7os,</p> <p>relativamente ao seguinte pedido:</p> </div> </br> <div class=\"req_details\"> <p><span id=\"client\">Cliente: </span>" + client + "</p> <p><span id=\"product\">Produto: </span>" + product + "</p> <p><span id=\"subject\">Assunto: </span>" + issue.subject + "</p> <p><span id=\"timestamp\">Data e Hora da submiss\u00E3o: </span>" + issue.start_date + "</p> <p><span id=\"operator\">Ticket atendido pelo operador: </span>" + issue.id + "</p> </div> </br></br> <h3 class=\"title\">Por favor indique o seu grau de satisfa\u00E7\u00E3o, </br>relativamente \u00E0 resolu\u00E7\u00E3o do ticket mencionado anteriormente:</h3> </br> <div class=\"inq\"> <div id=sat1><a href=\"http://www.google.pt/1\" class=\"links\">Totalmente insatisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat2><a href=\"http://www.google.pt/2\" class=\"links\">Insatisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat3><a href=\"http://www.google.pt/3\" class=\"links\">Nem insatisfeito nem satisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat4><a href=\"http://www.google.pt/4\" class=\"links\">Satisfeito:<span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat5><a href=\"http://www.google.pt/5\" class=\"links\">Totalmente satisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span></a></div> </br> </div> </br> <div id=\"goodbye\"> <p>Obrigado pela colabora\u00E7\u00E3o,</br> os melhores cumprimentos,</br>departamento de qualidade da JIBS.</p> </div></body></p></html>";
+                                    let body = "<html><head> <meta charset=\"UTF-8\"> <title>Inqu\u00E9rito de Satisfa\u00E7\u00E3o</title> <!-- Font Awesome Icon Library --> <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"> <style> .title { text-align: center; font-family: Arial, Helvetica, sans-serif; } #presentation { /*border-style: solid;*/ width: 50%; margin-right: auto; margin-left: auto; } #name { font-style: italic; font-weight: bold; } .req_details { /*border-style: solid;*/ width: 50%; margin-right: auto; margin-left: auto; } #client, #product, #subject, #timestamp, #operator { color: red; font-weight: bold; } .inq { /*border-style: solid;*/ width: 30%; margin-right: auto; margin-left: auto; padding-right: 10%; text-align: right; } .links { color: black; text-decoration: none; } .checked { color: orange; } #goodbye { text-align: right; margin-right: 10%; } </style></head><body> <h1 class=\"title\">Inqu\u00E9rio de Satisfa\u00E7\u00E3o - Sistema de Gest\u00E3o de Qualidade</h1> </br> <div id=\"presentation\"> <p>Caro(a) <span id=\"name\">" + user.firstname + " " + user.lastname + "</span>,</p> <p>este inqu\u00E9rito visa avaliar a qualidade do servi\u00E7o de apoio ao cliente da nossa empresa - <b>JIBS</b>,</p> <p>tendo por base a sua experi\u00EAncia no \u00E2mbito dos diferentes contactos feitos com os nossos servi\u00E7os,</p> <p>relativamente ao seguinte pedido:</p> </div> </br> <div class=\"req_details\"> <p><span id=\"client\">Cliente: </span>" + client + "</p> <p><span id=\"product\">Produto: </span>" + product + "</p> <p><span id=\"subject\">Assunto: </span>" + issue.subject + "</p> <p><span id=\"timestamp\">Data e Hora da submiss\u00E3o: </span>" + issue.start_date + "</p> <p><span id=\"operator\">Ticket atendido pelo operador: </span>" + issue.id + "</p> </div> </br></br> <h3 class=\"title\">Por favor indique o seu grau de satisfa\u00E7\u00E3o, </br>relativamente \u00E0 resolu\u00E7\u00E3o do ticket mencionado anteriormente:</h3> </br> <div class=\"inq\"> <div id=sat1><a href=\"" + uiBaseURL + "/submission?issueId=" + issue.id + "&score=1\" class=\"links\">Totalmente insatisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat2><a href=\"" + uiBaseURL + "/submission?issueId=" + issue.id + "&score=2\" class=\"links\">Insatisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat3><a href=\"" + uiBaseURL + "/submission?issueId=" + issue.id + "&score=3\" class=\"links\">Nem insatisfeito nem satisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat4><a href=\"" + uiBaseURL + "/submission?issueId=" + issue.id + "&score=4\" class=\"links\">Satisfeito:<span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star\"></span></a></div> </br> <div id=sat5><a href=\"" + uiBaseURL + "/submission?issueId=" + issue.id + "&score=5\" class=\"links\">Totalmente satisfeito: <span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span><span class=\"fa fa-star checked\"></span></a></div> </br> </div> </br> <div id=\"goodbye\"> <p>Obrigado pela colabora\u00E7\u00E3o,</br> os melhores cumprimentos,</br>departamento de qualidade da JIBS.</p> </div></body></p></html>";
 
                                     var mailOptions = {
                                         from: 'spamppcin@gmail.com',
                                         to: 'spamppcin@gmail.com',
-                                        subject: 'Sending Email using Node.js',
+                                        subject: 'Inquérito de Satisfação',
                                         html: body
                                     };
 
@@ -172,7 +173,7 @@ app.get("/globalIndicators", function(req, res) {
 // Endpoint that gets the evaluation of the user
 app.get("/evaluation/:id", function(req, res) {
     let id = req.params.id;
-    let evaluation = req.query.evaluation;
+    let score = req.query.score;
 
-    res.send(evaluation);
+    res.send(score);
 });
--- webapp/src/components/Submission/Submission.js
+++ webapp/src/components/Submission/Submission.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../../configs';
+const axios = require('axios');
 
-const Submission = () => {
+const Submission = (props) => {
 
     const logdata = {
         fontSize: "30px",
@@ -12,47 +14,68 @@ const Submission = () => {
         borderStyle: "dotted"
     }
 
-    const isOk = false;
 
-    if (isOk) {
-        return (
-            <div className="App">
-                <div className="container-fluid">
-                    <div className="text-center">
-                        <h1 id="title">Inquério de Satisfação - Sistema de Gestão de Qualidade</h1><br />
-                        <div id="head">
-                            <p>Agradecemos a sua colaboração na resposta a este inquérito de satisfação.</p><br />
-                            <p>A nossa plataforma recebeu com sucesso a sua resposta.</p>
-                        </div><br /><br />
-                        <div style={logdata}> 2019-04-10 / 21:23:47 </div>
-                        <br /><br />
-                        <div id="goodbye">
-                            <p>Obrigado pela sua colaboração!<br /><br />O departamento de qualidade da JIBS.</p>
+    const params = new URLSearchParams(props.location.search);
+    const score = params.get('score'); 
+    const issueId = params.get('issueId');
+
+    console.log(score);
+
+    const [evaluation, setEvaluation] = useState([]);
+
+    const fetchEvaluation = async () => {
+        const data = await fetch(global.apiBaseURL + "/evaluation/" + issueId + "?score=" + score).then(dataPromise => dataPromise.json()).then(data => {
+            setEvaluation(true);
+        });
+    };
+
+    fetchEvaluation();
+
+    if(evaluation == true) {
+        const isOk = false;
+        if (isOk) {
+            return (
+                <div className="App">
+                    <div className="container-fluid">
+                        <div className="text-center">
+                            <h1 id="title">Inquério de Satisfação - Sistema de Gestão de Qualidade</h1><br />
+                            <div id="head">
+                                <p>Agradecemos a sua colaboração na resposta a este inquérito de satisfação.</p><br />
+                                <p>A nossa plataforma recebeu com sucesso a sua resposta.</p>
+                            </div><br /><br />
+                            <div style={logdata}> 2019-04-10 / 21:23:47 </div>
+                            <br /><br />
+                            <div id="goodbye">
+                                <p>Obrigado pela sua colaboração!<br /><br />O departamento de qualidade da JIBS.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
-    }
+            );
+        }
+        else {
+            return (
+                <div className="App">
+                    <div className="container-fluid">
+                        <div className="text-center">
+                            <h1 id="title">Inquério de Satisfação - Sistema de Gestão de Qualidade</h1><br />
+                            <div id="head">
+                                <p>Agradecemos a sua colaboração na resposta a este inquérito de satisfação.</p><br />
+                                <p>Já for registado anteriormente o seu nível de satisfação a este inquérito.</p>
+                            </div><br /><br />
+                            <div style={logdata}>Não é possivel submeter duas vezes o seu nível de satisfação ou alterar o mesmo!</div>
+                            <br /><br />
+                            <div id="goodbye">
+                                <p>Obrigado pela sua colaboração!<br /><br />O departamento de qualidade da JIBS.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    } 
     else {
-        return (
-            <div className="App">
-                <div className="container-fluid">
-                    <div className="text-center">
-                        <h1 id="title">Inquério de Satisfação - Sistema de Gestão de Qualidade</h1><br />
-                        <div id="head">
-                            <p>Test</p><br />
-                            <p>A nossa plataforma recebeu com sucesso a sua resposta.</p>
-                        </div><br /><br />
-                        <div style={logdata}> 2019-04-10 / 21:23:47 </div>
-                        <br /><br />
-                        <div id="goodbye">
-                            <p>Obrigado pela sua colaboração!<br /><br />O departamento de qualidade da JIBS.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+        return (<h1>Loading...</h1>);
     }
 }
 
